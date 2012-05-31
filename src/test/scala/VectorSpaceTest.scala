c44a8e27cb0c826bb97385ecf53e2104397e350d
Le Huy <lehuy20@gmail.com>
2012-05-31 20:08:09 +0200

    test normalized tf vector passed

--- src/test/scala/VectorSpaceTest.scala
+++ src/test/scala/VectorSpaceTest.scala
@@ -1,22 +1,29 @@
 import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
  
-class VectorSpaceTest extends FunSuite {
+class VectorSpaceTest extends FunSuite with BeforeAndAfter {
  
-  test("Figure 6.11") {
-     val vs = new VectorSpace[Int]
+  var vs = new VectorSpace[Int]
 
+  before {
      val doc1 = "car "*27 + "auto "*3 + "best "*14
      val doc2 = "car "*4  + "auto "*33 + "insurance "*33
      val doc3 = "car "*24 + "insurance "*29 + "best "*17
 
-     Map(1->doc1,
-         2->doc2,
-         3->doc3).foreach{ case (docId,str)=>
+     Map(1->doc1, 2->doc2, 3->doc3).foreach{ case (docId,str)=>
        vs.vectorize(docId,str.split(" "))
      }
+  }
 
-     expect(Array("auto","best","car").toList){vs.docIndex(1).terms.toList }
-     expect(Array(3,14,27).toList){vs.docIndex(1).termFreqs.toList }
+  test("term freqs") {
+     expect(List("auto","best","car")){vs.docIndex(1).terms.toList }
+     expect(List(3,14,27)){vs.docIndex(1).termFreqs.toList }
+  }
+
+  test("normalized term freqs") {
+     expect(List(0.1,0.46,0.88).map{ntf=>math.round(ntf*vs.docIndex(1).vectorLen)}){
+       vs.docIndex(1).termFreqs.toList
+     }
   }
  
 }
