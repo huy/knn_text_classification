2cfdb3d2fc99b9b99160bab030ac3a0c0c6d0686
Le Huy <lehuy20@gmail.com>
2012-06-10 23:52:40 +0200

    train test doc after apply in KNN

--- src/main/scala/CodeTable.scala
+++ src/main/scala/CodeTable.scala
@@ -34,38 +34,6 @@ class CodeTable {
   }
 }
 
-class NaiveBayesEnricher(var codeTable: CodeTable){
-  var classifier = new NaiveBayes[String]
-
-  codeTable.codeDefSeq.foreach {codeDef =>
-    classifier.train(klass = codeDef.id, doc = codeDef.termSeq)
-  }
-
-  def enrich(unknown: CodeDef): Unit = {
-    val (id,score) = classifier.apply(unknown.termSeq) 
-    codeTable.codeDef(id).merge(unknown)
-    classifier.train(klass = id, doc = unknown.termSeq)
-  }
-}
-
-class KNNEnricher(var codeTable: CodeTable, val k:Int = 2) {
-  var corpus = new Corpus
-  var classifier  = new KNN[String](corpus)
- 
-  codeTable.codeDefSeq.foreach {codeDef=>
-    val docId = corpus.add(codeDef.termSeq)
-    classifier.train(klass = codeDef.id, docId = docId)
-  } 
-
-  def enrich(codeDef: CodeDef): Unit = {
-    val docId = corpus.add(codeDef.termSeq)
-
-    val id = classifier.apply(docId,k) 
-    codeTable.codeDef(id).merge(codeDef)
-  }
-
-}
-
 object CodeTable {
 
   def parseText(lines: Iterator[String]): CodeTable = {
