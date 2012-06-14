b5babbae9a15bb22cd12bb1b2f2d18cbe2f4062c
Le Huy <lehuy20@gmail.com>
2012-06-14 20:10:19 +0200

    correct KNN Test after modifying KNN algorithm to use sum(distance) per class in K neighbours for making decision

--- src/test/scala/KNNTest.scala
+++ src/test/scala/KNNTest.scala
@@ -6,7 +6,7 @@ class KNNTest extends FunSuite with BeforeAndAfter {
   test("Knn"){
     val corpus = new Corpus
     val doc1 = "car "*27 + "auto "*3 + "best "*14
-    val doc2 = "car "*4  + "auto "*33 + "insurance "*33
+    val doc2 = "car "*4  + "auto "*33 + "insurance "*33 
     val doc3 = "car "*24 + "insurance "*29 + "best "*17
 
     val docId1 = corpus.add(doc1.split(" "))
@@ -18,7 +18,7 @@ class KNNTest extends FunSuite with BeforeAndAfter {
     knn.train(sample = docId1, klass = "car")
     knn.train(sample = docId2, klass = "insurance")
 
-    expect("car"){
+    expect("insurance"){
        knn.apply(test = docId3, k=2)
     }
   }
